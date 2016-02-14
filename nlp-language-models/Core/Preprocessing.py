c82bc3481f5509d01f5b396f20731daa77a0f272
Luis Felipe Hernandez <lf.hernandez@outlook.com>
2016-02-14 16:57:07 -0500

    Added smart sentence segmentor

--- nlp-language-models/Core/Preprocessing.py
+++ nlp-language-models/Core/Preprocessing.py
@@ -4,23 +4,33 @@ import re
 def dumb_sentence_segmenter(file):
     corpus = open(file, 'r')
     lines = corpus.read().split('.')
-    # segmentedList = []
-    # for line in lines:
-    #     segmentedList.append(re.split(r'[.,;:_!*()\[\]?\"\-\n]', line))
     return lines
 
+def smart_sentence_segmenter(file):
+    corpus = open(file, 'r')
+    segmentedList = []
+    for line in corpus:
+        segmentedList.append(re.split(r'[.,;:_!*()\[\]?\"\-\n]', line))
+    return segmentedList
 
 def dumb_tokenizer(sentence):
-    tokens = sentence.split()
+    # TODO account for empty arrays
+    tokens = []
+    for i in range(len(sentence) - 1):
+        tokens = sentence[i].split()
     return tokens
 
 
 def main():
     file = "test.txt"
-    corpus = (dumb_sentence_segmenter(file))
+    corpus = smart_sentence_segmenter(file)
+    # for line in corp:
+    #     print(line)
+    # corpus = (dumb_sentence_segmenter(file))
     tokens = []
     for line in corpus:
         tokens.append(dumb_tokenizer(line))
+
     for token in tokens:
         print(token)
 
