89ecd2c93d89362ca69b49fa97b35598e35c8b4a
Luis Felipe Hernandez <luis.f.hernandez@stonybrook.edu>
2016-02-16 18:42:38 -0500

    Added Absolute Discounting and fixed math in estimation methods

--- nlp-language-models/Core/BigramLanguageModel.py
+++ nlp-language-models/Core/BigramLanguageModel.py
@@ -1,22 +1,29 @@
 import math
 import re
 
-text = "This ratio is called This ratio is called the relative frequency the use of relative frequency as a way to esitmate This ratio"
+text = "A fat dog is a fat dog only when other fat dogs say he is a fat dog"
+
 
 def compute_bigram_probability():
-    probability = 0.0
+    # running total of estimation method probabilities
+    mlep = 0.0
     lsp = 0.0
+    adp = 0.0
+
     augmented_text = add_edge_markers(text)
     corpus = augmented_text.lower().split()
     bigram_set = generate_bigram_list(corpus)
+
     for bigram in bigram_set:
         bigram = bigram.split()
         mle = maximum_likelihood_estimate(bigram[0], bigram[1], bigram_set, corpus)
-        ls = laplace_smoothin(bigram[0], bigram[1], bigram_set, corpus)
-        print("P(" + bigram[0] + " | " + bigram[1] + ")" + " = " +str(mle))
-        probability += math.log(mle)
-        lsp += math.log(ls)
-    print("mle = " + str(2**probability) + "\nls=" + str(2**lsp))
+        ls = laplace_smoothing(bigram[0], bigram[1], bigram_set, corpus)
+        ads = absolute_discounting(bigram[0], bigram[1], bigram_set, corpus)
+        print("P(" + bigram[0] + " | " + bigram[1] + ")" + " = " + str(mle))
+        mlep += math.log(mle, 2)
+        lsp += math.log(ls, 2)
+        adp += math.log(ads, 2)
+    print("mle = " + str(2 ** mlep) + "\nls = " + str(2 ** lsp) + "\nabsolute discounting = " + str(2**adp))
 
 
 def add_edge_markers(corpus):
@@ -27,15 +34,14 @@ def generate_bigram_list(corpus):
     bigrams = []
     for word in range(len(corpus) - 1):
         current = corpus[word] + " " + corpus[word + 1]
-        if current not in bigrams:
-            bigrams.append(current)
+        bigrams.append(current)
     return bigrams
 
 
 def count_ngram_occurence(ngram, set):
     count = 0
-    for current_ngram in set:
-        if current_ngram == ngram:
+    for currentngram in set:
+        if currentngram == ngram:
             count += 1
     return count
 
@@ -45,14 +51,28 @@ def generate_bigram(x):
     x = x[0] + " " + x[1]
 
 
-def maximum_likelihood_estimate(x, y, bigram_set, corpus):
+def maximum_likelihood_estimate(x, y, bigramset, corpus):
     bigram = x + ' ' + y
-    return count_ngram_occurence(bigram, bigram_set ) / count_ngram_occurence(x, corpus)
+    return count_ngram_occurence(bigram, bigramset) / count_ngram_occurence(x, corpus)
 
 
-
-def laplace_smoothin(x,y, bigram_set, corpus):
-    vocabulary = print(len(re.findall('\w+', corpus)))
+def laplace_smoothing(x, y, bigramset, corpus):
     bigram = x + ' ' + y
-    return (count_ngram_occurence(bigram, bigram_set ) + 1) / (count_ngram_occurence(x, corpus) + vocabulary)
+    return (count_ngram_occurence(bigram, bigramset) + 1) / (
+    count_ngram_occurence(x, corpus) + len(get_vocabulary(corpus)))
+
+
+def absolute_discounting(x, y, bigram_set, corpus):
+    bigram = x + ' ' + y
+    return (count_ngram_occurence(bigram, bigram_set) - 0.5)/ count_ngram_occurence(x, corpus)
+
+
+def get_vocabulary(corpus):
+    distinct = []
+    for word in corpus:
+        if word not in distinct:
+            distinct.append(word)
+    return distinct
+
+
 compute_bigram_probability()
